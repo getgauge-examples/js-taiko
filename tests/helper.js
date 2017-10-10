88244fe3f63877f89cdc3b5bdb1f6b6e8e306f75
kashishm <kashishmunjal64@gmail.com>
2017-10-10 14:46:46 +0530

    Adding textField

--- tests/helper.js
+++ tests/helper.js
@@ -64,35 +64,41 @@ const waitUntil = async(condition, options = { intervalTime: 1000, timeout: 1000
 
 const $ = (selector) => {
     const get = async() => await (selector.startsWith("//") ? xpath(selector) : page.$(selector));
-    return { get: get, exists: async() => (await get()) != null, };
+    return { get: get, exists: exists(get), };
 }
 
 const image = (selector) => {
     assertType(selector);
     const get = async() => await page.$(`img[alt="${selector}"]`);
-    return { get: get, exists: async() => (await get()) != null, };
+    return { get: get, exists: exists(get), };
 }
 
 const link = (selector) => {
     const get = async() => await getElementByTag(selector, "a");
-    return { get: get, exists: async() => (await get()) != null, };
+    return { get: get, exists: exists(get), };
 }
 
 const listItem = (selector) => {
     const get = async() => await getElementByTag(selector, "li");
-    return { get: get, exists: async() => (await get()) != null, }
+    return { get: get, exists: exists(get), };
+}
+
+const textField = (selector, attribute = "placeholder") => {
+    assertType(selector);
+    const get = async() => await page.$(`input[${attribute}="${selector}"]`);
+    return { get: get, exists: exists(get), value: async() => await page.evaluate(e => e.value, await get()) }
 }
 
 const text = (text) => {
     assertType(text);
     const get = async(element = "*") => await xpath(`//*[text()="${text}"]`.replace("*", element));
-    return { get: get, exists: async() => (await get()) != null, };
+    return { get: get, exists: exists(get), };
 }
 
 const contains = (text) => {
     assertType(text);
     const get = async(element = "*") => await xpath(`//*[contains(.,"${text}")]`.replace("*", element));
-    return { get: get, exists: async() => (await get()) != null, }
+    return { get: get, exists: exists(get), };
 }
 
 const getElement = async(selector) => {
@@ -147,23 +153,26 @@ const sleep = (milliseconds) => {
         if ((new Date().getTime() - start) > milliseconds) break;
 }
 
+const exists = (get) => async() => (await get()) != null;
+
 const dummy = (e) => e;
 
 module.exports = {
     goto,
     openBrowser,
     closeBrowser,
+    $,
+    link,
+    listItem,
+    textField,
     image,
     click,
     doubleClick,
     write,
     press,
     text,
-    $,
     contains,
     hover,
-    link,
-    listItem,
     waitUntil,
     to: dummy,
     into: dummy,
--- tests/step_implementation.js
+++ tests/step_implementation.js
@@ -1,35 +1,26 @@
 /* globals gauge*/
 "use strict";
 const assert = require("assert");
-const { goto, openBrowser, closeBrowser, image, click, doubleClick, write, press, text, $, contains, hover, into, to, link, listItem, waitUntil, keys } = require("./helper");
+const {
+    goto, openBrowser, closeBrowser, $, link, listItem, textField, image, click,
+    doubleClick, write, press, text, contains, hover, waitUntil, to, into, keys,
+} = require("./helper");
 
 beforeSuite(async() => await openBrowser());
 
 afterSuite(async() => await closeBrowser());
 
-step("Navigate to Gauge homepage <query>", async function(query) {
-    await goto(query);
-});
+step("Navigate to Gauge homepage <query>", async query => await goto(query));
 
-step("Display the Gauge logo", async function() {
-    assert.ok(await image("Gauge logo").exists());
-});
+step("Display the Gauge logo", async() => assert.ok(await image("Gauge logo").exists()));
 
-step("Go to Gauge get started page", async function() {
-    await click("Get Started");
-});
+step("Go to Gauge get started page", async() => await click("Get Started"));
 
-step("Display the sub title <title>", async function(title) {
-    assert.ok(await text(title).exists());
-});
+step("Display the sub title <title>", async(title) => assert.ok(await text(title).exists()));
 
-step("Go to Gauge documentation page", async function() {
-    await click($(`//*[text()="Documentation"]`));
-});
+step("Go to Gauge documentation page", async() => await click($(`//*[text()="Documentation"]`)));
 
-step("Display the Gauge version", async function() {
-    assert.ok(await contains("0.9.3").exists());
-});
+step("Display the Gauge version", async() => assert.ok(await contains("0.9.3").exists()));
 
 step("Go to plugins page", async function() {
     await hover("Get Started");
@@ -48,7 +39,9 @@ step("Display the official plugins", async function() {
 });
 
 step("Search for Hooks", async function() {
-    await write("Hooks", into($("input[placeholder='Search docs']")));
+    await write("Hooks", into(textField("Search docs")));
+    assert.equal(await textField("Search docs").value(), "Hooks")
     await press(keys.ENTER);
+
     await waitUntil(link("Language Features").exists);
 });
